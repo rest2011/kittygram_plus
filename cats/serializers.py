20347a47e74d8c1b5615992129756f2d7f0ce72e
Ринат Хаматьяров <hamatrin@gmail.com>
2023-01-30 01:19:46 +0500

    API

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,22 +1,51 @@
+import datetime as dt
+
+import webcolors
 from rest_framework import serializers
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()  # Вот он - наш собственный тип поля
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year 
 
 
     def create(self, validated_data):
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,9 +1,8 @@
+from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from django.urls import include, path
-
 from cats.views import CatViewSet, OwnerViewSet
-
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
@@ -11,4 +10,9 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
